09c781925d3202aca83a9c53232b6f8060f7a9e6
mohaameedawad <mohammedawaad26@gmail.com>
2024-09-26 18:29:05 +0300

    installing bootstrap and primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,19 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
     data: { title: 'Home' },
   },
   {
     path: 'list',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/list/list.component').then((m) => m.ListComponent),
     data: { title: 'List' },
   },
   {
     path: '**',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/not-found/not-found.component').then(
         (m) => m.NotFoundComponent
       ),
