124cb1618e2f081632c2b5c1e621d15c22a4c570
mohaameedawad <mohammedawaad26@gmail.com>
2024-09-26 17:22:07 +0300

    "Added routes configuration for home, list, and not found pages"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+    data: { title: 'Home' },
+  },
+  {
+    path: 'list',
+    loadChildren: () =>
+      import('./pages/list/list.component').then((m) => m.ListComponent),
+    data: { title: 'List' },
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+    data: { title: 'Not Found' },
+  },
+];
